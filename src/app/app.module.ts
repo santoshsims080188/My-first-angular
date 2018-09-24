bdf90ba2e53a041e32629691758d63f0bac82737
Your Namepavankasa <pavankk@msn.com>
2018-09-24 13:23:21 +0530

    router links updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import{FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NgForDirComponent } from './ng-for-dir/ng-for-dir.component';
 import { SytlechangeDirective } from './sytlechange.directive';
+import {RouterModule, Routes} from '@angular/router';
+import {TemplateFormsComponent} from './template-forms/template-forms.component';
+import {ReactiveFormsComponent} from './reactive-forms/reactive-forms.component'
+const appRoutes : Routes= [
+  {path:'', component:AppComponent},
+  {path:'Directives', component:NgForDirComponent},
+  {path:'TemplatesForms', component:TemplateFormsComponent},
+  {path:'ReactiveForms', component:ReactiveFormsComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NgForDirComponent,
-    SytlechangeDirective
+    SytlechangeDirective,
+    TemplateFormsComponent,
+    ReactiveFormsComponent,
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, RouterModule.forRoot(
+      appRoutes,{enableTracing:true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
